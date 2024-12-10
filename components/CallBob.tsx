98fc97e9633c1895d67ed0d604e0750ac006b36b
Scott Ogden <scottogden@Scotts-MacBook-Air.local>
2024-12-11 06:50:00 +0900

    Updated the app with local changes

--- components/CallBob.tsx
+++ components/CallBob.tsx
@@ -34,7 +34,6 @@ export default function CallBob() {
             endCall={endCall}
             isChatbotSpeaking={isChatbotSpeaking}
           />
-          <ConversionIdea onSelect={handleSend} />
         </div>
       </div>
     </div>
